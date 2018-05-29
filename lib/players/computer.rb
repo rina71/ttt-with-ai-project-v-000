3e3c604d6f20f3c4a41339d77462cfd4b46e51a3
Yurina Shin <yurinas@gmail.com>
2018-05-29 07:13:25 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -16,26 +16,18 @@ module Players
     end
 
     def move(board)
-      a = Random.new
-      input = " "
-      until board.valid_move?(input)
-        input = "#{a.rand(1..9)}"
-      if board.cells[0] == board.cells[1]
-        input = board.cells[2]
+      # a = Random.new
+      # input = " "
+      # until board.valid_move?(input)
+      #   input = "#{a.rand(1..9)}"
+    
+    
 
-      if board.cells[3] == board.cells[4]
-        input = board.cells[5]
-
-      if board.cells[6] == board.cells[7]
-        input = board.cells[8]
-      end
-    end
-  end
-        # best_moves = ["8","1","6","4","3","9","2","7","5"]
-        # # input = " "
-        # best_moves.each{|a| if board.valid_move?(a)
-        #    input = a
-        #  end}
+        best_moves = ["8","1","6","4","3","9","2","7","5"]
+        # input = " "
+        best_moves.each{|a| if board.valid_move?(a)
+           input = a
+         end}
        end
       # end
       input
