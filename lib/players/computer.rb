7d782482fa7fb664eac2a8c2a59b176350e7c3a0
Yurina Shin <yurinas@gmail.com>
2018-05-29 07:10:37 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -26,6 +26,9 @@ module Players
       if board.cells[3] == board.cells[4]
         input = board.cells[5]
       end
+      if board.cells[6] == board.cells[7]
+        input = board.cells[8]
+      end
       else
         # best_moves = ["8","1","6","4","3","9","2","7","5"]
         # # input = " "
